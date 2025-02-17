38025c71079961f6e62fb0e5453370893c787052
FAheem <fnaseem010@gmail.com>
2025-02-18 00:31:27 +0530

    1.3

---
+++ IntroductionToSpringBoot/src/main/java/com/SpringbootCourse/IntroductionToSpringBoot/Apple.java
@@ -0,0 +1,10 @@
+package com.SpringbootCourse.IntroductionToSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+//@Component
+public class Apple {
+    void getApple(){
+        System.out.println("I am Apple");
+    }
+}
